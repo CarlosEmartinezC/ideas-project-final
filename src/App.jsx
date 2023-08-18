53740128ab325b5f9a2dc4f20a76569805c27191
neyryvargasd <neyry007@gmail.com>
2023-08-18 18:04:25 -0500

    merge Amaya

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './core/layouts/header-components/header';
-
+import Footer from './core/layouts/footer';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
          <>
          
          </>
-      
+        <Footer/>
       
       
     </div>
