edca148dd67ccab040cb58e015340cdc71a2e384
CarlosEmartinezC <133069933+CarlosEmartinezC@users.noreply.github.com>
2023-08-17 22:09:25 -0500

    avances json

---
+++ src/core/services/data.js
@@ -0,0 +1,278 @@
+const data = [ 
+    {
+    primer_Grado: {
+            dba: [
+                {
+                    Id: 1,
+                    Descripción: "descripcion corta",
+                    Descripción: "Comprende que los sentidos le permiten percibir algunas características de los objetos que nos rodean (temperatura, sabor, sonidos, olor, color, texturas y formas).",
+                    content: [
+                        {
+                         evidencia: "Describe y caracteriza, utilizando el sentido apropiado, sonidos, sabores, olores, colores, texturas y formas" 
+                        },
+                        {
+                         evidencia: "Compara y describe cambios en las temperaturas (más caliente, similar, menos caliente) utilizando el tacto en diversos objetos (con diferente color) sometidos a fuentes de calor como el sol. "
+                        },
+                        {
+                         evidencia: "Describe y caracteriza, utilizando la vista, diferentes tipos de luz (color, intensidad y fuente)."
+                        },
+                        {
+                         evidencia: "Usa instrumentos como la lupa para realizar observaciones de objetos pequeños y representarlos mediante dibujos."
+                        }
+                    ]
+                }, {
+                    Id: 2,
+                    Descripción: "descripcion corta DBA2",
+                    Descripción: "Comprende que existe una gran variedad de materiales y que éstos se utilizan para distintos fines, según sus características (longitud, dureza, flexibilidad, permeabilidad al agua, solubilidad, ductilidad, maleabilidad, color, sabor, textura).",
+                    content: [
+                        {
+                            evidencia: "Clasifica materiales de uso cotidiano a partir de características que percibe con los sentidos, incluyendo materiales sólidos como madera, plástico, vidrio, metal, roca y líquidos como opacos, incoloros, transparentes, así como algunas propiedades (flexibilidad, dureza, permeabilidad al agua, color, sabor y textura)." 
+                           },
+                           {
+                            evidencia: "Predice cuáles podrían ser los posibles usos de un material (por ejemplo, la goma), de acuerdo con sus características. "
+                           },
+                           {
+                            evidencia: "Selecciona qué materiales utilizaría para fabricar un objeto dada cierta necesidad (por ejemplo, un paraguas que evite el paso del agua). "
+                           },
+                           {
+                            evidencia: "Utiliza instrumentos no convencionales (sus manos, palos, cuerdas, vasos, jarras) para medir y clasificar materiales según su tamaño."
+                           }
+                    ]
+                }, {
+                    Id: 3,
+                    Descripción: "descripcion corta DBA3",
+                    Descripción: "Comprende que los seres vivos (plantas y animales) tienen características comunes (se alimentan, respiran, tienen un ciclo de vida, responden al entorno) y los diferencia de los objetos inertes.",
+                    content: [
+                        {
+                            evidencia: "Clasifica seres vivos (plantas y animales) de su entorno, según sus características observables (tamaño, cubierta corporal, cantidad y tipo de miembros, forma de raíz, tallo, hojas, flores y frutos) y los diferencia de los objetos inertes, a partir de criterios que tienen que ver con las características básicas de los seres vivos." 
+                           },
+                           {
+                            evidencia: "Compara características y partes de plantas y animales, utilizando instrumentos simples como la lupa para realizar observaciones."
+                           },
+                           {
+                            evidencia: "Describe las partes de las plantas (raíz, tallo, hojas, flores y frutos), así como las de animales de su entorno, según características observables (tamaño, cubierta corporal, cantidad y tipo de miembros)."
+                           },
+                           {
+                            evidencia: "Propone acciones de cuidado a plantas y animales, teniendo en cuenta características como tipo de alimentación, ciclos de vida y relación con el entorno."
+                           }
+                    ]
+                }, {
+                    Id: 4,
+                    Descripción: "descripcion corta DBA4",
+                    Descripción: "Comprende que su cuerpo experimenta constantes cambios a lo largo del tiempo y reconoce a partir de su comparación que tiene características similares y diferentes a las de sus padres y compañeros.",
+                    content: [
+                        {
+                            evidencia: "Registra cambios físicos ocurridos en su cuerpo durante el crecimiento, tales como peso, talla, longitud de brazos, piernas, pies y manos, así como algunas características que no varían como el color de ojos, piel y cabello." 
+                           },
+                           {
+                            evidencia: "Describe su cuerpo y predice los cambios que se producirán en un futuro, a partir de los ejercicios de comparación que realiza entre un niño y un adulto."
+                           },
+                           {
+                            evidencia: "Describe y registra similitudes y diferencias físicas que observa entre niños y niñas de su grado reconociéndose y reconociendo al otro."
+                           },
+                           {
+                            evidencia: "Establece relaciones hereditarias a partir de las características físicas de sus padres, describiendo diferencias y similitudes."
+                           }
+                    ] 
+                }
+            ], 
+            estandares[
+                {
+                    curso: 1,
+                    img: "",
+                },{
+                    curso: 2,
+                    img: "",
+
+                },{
+                    curso: 3,
+                    img: "",
+                }
+            ]
+        }
+    }, {
+    segundo_Grado {
+        dba: [
+            {
+                Id: 1,
+                Descripción: "descripcion corta",
+                Descripción: "Comprende que una acción mecánica (fuerza) puede producir distintas deformaciones en un objeto, y que este resiste a las fuerzas de diferente modo, de acuerdo con el material del que está hecho.",
+                content: [
+                    {
+                     evidencia: "Compara los cambios de forma que se generan sobre objetos constituidos por distintos materiales (madera, hierro, plástico, plastilina, resortes, papel, entre otros), cuando se someten a diferentes acciones relacionadas con la aplicación de fuerzas (estirar, comprimir, torcer, aplastar, abrir, partir, doblar, arrugar)." 
+                    },
+                    {
+                     evidencia: "Clasifica los materiales según su resistencia a ser deformados cuando se les aplica una fuerza."
+                    },
+                    {
+                     evidencia: "Predice el tipo de acción requerida para producir una deformación determinada en un cierto material y las comunica haciendo uso de diferentes formatos (oral, escrito)."
+                    }
+                ]
+            }, {
+                Id: 2,
+                Descripción: "descripcion corta DBA2",
+                Descripción: "Comprende que las sustancias pueden encontrarse en distintos estados (sólido, líquido y gaseoso).",
+                content: [
+                    {
+                        evidencia: "Clasifica materiales de su entorno según su estado (sólidos, líquidos o gases) a partir de sus propiedades básicas (si tienen forma propia o adoptan la del recipiente que los contiene, si fluyen, entre otros)." 
+                       },
+                       {
+                        evidencia: "Predice cuáles podrían ser los posibles usos de un material (por ejemplo, la goma), de acuerdo con sus características. "
+                       },
+                       {
+                        evidencia: "Selecciona qué materiales utilizaría para fabricar un objeto dada cierta necesidad (por ejemplo, un paraguas que evite el paso del agua). "
+                       },
+                       {
+                        evidencia: "Utiliza instrumentos no convencionales (sus manos, palos, cuerdas, vasos, jarras) para medir y clasificar materiales según su tamaño."
+                       }
+                ]
+            }, {
+                Id: 3,
+                Descripción: "descripcion corta DBA3",
+                Descripción: "Comprende que los seres vivos (plantas y animales) tienen características comunes (se alimentan, respiran, tienen un ciclo de vida, responden al entorno) y los diferencia de los objetos inertes.",
+                content: [
+                    {
+                        evidencia: "Clasifica seres vivos (plantas y animales) de su entorno, según sus características observables (tamaño, cubierta corporal, cantidad y tipo de miembros, forma de raíz, tallo, hojas, flores y frutos) y los diferencia de los objetos inertes, a partir de criterios que tienen que ver con las características básicas de los seres vivos." 
+                       },
+                       {
+                        evidencia: "Compara características y partes de plantas y animales, utilizando instrumentos simples como la lupa para realizar observaciones."
+                       },
+                       {
+                        evidencia: "Describe las partes de las plantas (raíz, tallo, hojas, flores y frutos), así como las de animales de su entorno, según características observables (tamaño, cubierta corporal, cantidad y tipo de miembros)."
+                       },
+                       {
+                        evidencia: "Propone acciones de cuidado a plantas y animales, teniendo en cuenta características como tipo de alimentación, ciclos de vida y relación con el entorno."
+                       }
+                ]
+            }, {
+                Id: 4,
+                Descripción: "descripcion corta DBA4",
+                Descripción: "Comprende que su cuerpo experimenta constantes cambios a lo largo del tiempo y reconoce a partir de su comparación que tiene características similares y diferentes a las de sus padres y compañeros.",
+                content: [
+                    {
+                        evidencia: "Registra cambios físicos ocurridos en su cuerpo durante el crecimiento, tales como peso, talla, longitud de brazos, piernas, pies y manos, así como algunas características que no varían como el color de ojos, piel y cabello." 
+                       },
+                       {
+                        evidencia: "Describe su cuerpo y predice los cambios que se producirán en un futuro, a partir de los ejercicios de comparación que realiza entre un niño y un adulto."
+                       },
+                       {
+                        evidencia: "Describe y registra similitudes y diferencias físicas que observa entre niños y niñas de su grado reconociéndose y reconociendo al otro."
+                       },
+                       {
+                        evidencia: "Establece relaciones hereditarias a partir de las características físicas de sus padres, describiendo diferencias y similitudes."
+                       }
+                ] 
+            }
+        ],
+        estandares[
+            {
+                curso: 1,
+                img: "",
+            },{
+                curso: 2,
+                img: "",
+
+            },{
+                curso: 3,
+                img: "",
+            }
+
+        ]
+    }
+    }, 
+    tercer_Grado {
+        dba: [
+            {
+                Id: 1,
+                Descripción: "descripcion corta",
+                Descripción: "Comprende que los sentidos le permiten percibir algunas características de los objetos que nos rodean (temperatura, sabor, sonidos, olor, color, texturas y formas).",
+                content: [
+                    {
+                     evidencia: "Describe y caracteriza, utilizando el sentido apropiado, sonidos, sabores, olores, colores, texturas y formas" 
+                    },
+                    {
+                     evidencia: "Compara y describe cambios en las temperaturas (más caliente, similar, menos caliente) utilizando el tacto en diversos objetos (con diferente color) sometidos a fuentes de calor como el sol. "
+                    },
+                    {
+                     evidencia: "Describe y caracteriza, utilizando la vista, diferentes tipos de luz (color, intensidad y fuente)."
+                    },
+                    {
+                     evidencia: "Usa instrumentos como la lupa para realizar observaciones de objetos pequeños y representarlos mediante dibujos."
+                    }
+                ]
+            }, {
+                Id: 2,
+                Descripción: "descripcion corta DBA2",
+                Descripción: "Comprende que existe una gran variedad de materiales y que éstos se utilizan para distintos fines, según sus características (longitud, dureza, flexibilidad, permeabilidad al agua, solubilidad, ductilidad, maleabilidad, color, sabor, textura).",
+                content: [
+                    {
+                        evidencia: "Clasifica materiales de uso cotidiano a partir de características que percibe con los sentidos, incluyendo materiales sólidos como madera, plástico, vidrio, metal, roca y líquidos como opacos, incoloros, transparentes, así como algunas propiedades (flexibilidad, dureza, permeabilidad al agua, color, sabor y textura)." 
+                       },
+                       {
+                        evidencia: "Predice cuáles podrían ser los posibles usos de un material (por ejemplo, la goma), de acuerdo con sus características. "
+                       },
+                       {
+                        evidencia: "Selecciona qué materiales utilizaría para fabricar un objeto dada cierta necesidad (por ejemplo, un paraguas que evite el paso del agua). "
+                       },
+                       {
+                        evidencia: "Utiliza instrumentos no convencionales (sus manos, palos, cuerdas, vasos, jarras) para medir y clasificar materiales según su tamaño."
+                       }
+                ]
+            }, {
+                Id: 3,
+                Descripción: "descripcion corta DBA3",
+                Descripción: "Comprende que los seres vivos (plantas y animales) tienen características comunes (se alimentan, respiran, tienen un ciclo de vida, responden al entorno) y los diferencia de los objetos inertes.",
+                content: [
+                    {
+                        evidencia: "Clasifica seres vivos (plantas y animales) de su entorno, según sus características observables (tamaño, cubierta corporal, cantidad y tipo de miembros, forma de raíz, tallo, hojas, flores y frutos) y los diferencia de los objetos inertes, a partir de criterios que tienen que ver con las características básicas de los seres vivos." 
+                       },
+                       {
+                        evidencia: "Compara características y partes de plantas y animales, utilizando instrumentos simples como la lupa para realizar observaciones."
+                       },
+                       {
+                        evidencia: "Describe las partes de las plantas (raíz, tallo, hojas, flores y frutos), así como las de animales de su entorno, según características observables (tamaño, cubierta corporal, cantidad y tipo de miembros)."
+                       },
+                       {
+                        evidencia: "Propone acciones de cuidado a plantas y animales, teniendo en cuenta características como tipo de alimentación, ciclos de vida y relación con el entorno."
+                       }
+                ]
+            }, {
+                Id: 4,
+                Descripción: "descripcion corta DBA4",
+                Descripción: "Comprende que su cuerpo experimenta constantes cambios a lo largo del tiempo y reconoce a partir de su comparación que tiene características similares y diferentes a las de sus padres y compañeros.",
+                content: [
+                    {
+                        evidencia: "Registra cambios físicos ocurridos en su cuerpo durante el crecimiento, tales como peso, talla, longitud de brazos, piernas, pies y manos, así como algunas características que no varían como el color de ojos, piel y cabello." 
+                       },
+                       {
+                        evidencia: "Describe su cuerpo y predice los cambios que se producirán en un futuro, a partir de los ejercicios de comparación que realiza entre un niño y un adulto."
+                       },
+                       {
+                        evidencia: "Describe y registra similitudes y diferencias físicas que observa entre niños y niñas de su grado reconociéndose y reconociendo al otro."
+                       },
+                       {
+                        evidencia: "Establece relaciones hereditarias a partir de las características físicas de sus padres, describiendo diferencias y similitudes."
+                       }
+                ] 
+            }
+        ],
+        estandares[
+            {
+                curso: 1,
+                img: "",
+            },{
+                curso: 2,
+                img: "",
+
+            },{
+                curso: 3,
+                img: "",
+            }
+
+        ]
+    }, {
+        Cuarto grado [
+
+        ]
+    }
+]
