f7f99a375ea1c8a84e4312e26fdc745b133b727e
JSebastian08 <juansebastian0197@hotmail.com>
2023-08-23 22:38:12 -0500

    Ultima modificación de imagenes y tamaño del slider

--- src/pages/Cursos/components/slider.js
+++ src/pages/Cursos/components/slider.js
@@ -1,8 +1,8 @@
 import React from "react";
 import '../components/slider.css'
-import Imagen1 from "../../../recursos/imagen1.jpg";
-import Imagen2 from "../../../recursos/imagen2.jpg";
-import Imagen3 from "../../../recursos/imagen3.jpg";
+import Imagen1 from "../../../recursos/imagen1-06.jpg";
+import Imagen2 from "../../../recursos/imagen2-06.jpg";
+import Imagen3 from "../../../recursos/imagen3-06.jpg";
 
 const slider = () => {
     return(
@@ -10,13 +10,13 @@ const slider = () => {
             <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                    <img src={Imagen1} className="d-block" alt="..."/>
+                    <img src={Imagen1} className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                    <img src= {Imagen2} className="d-block" alt="..."/>
+                    <img src= {Imagen2} className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                    <img src= {Imagen3} className="d-block" alt="..."/>
+                    <img src= {Imagen3} className="d-block w-100" alt="..."/>
                     </div>
                 </div>
             </div>
