2ffcc6d29fc82b65e3366c8728086ce61c81db05
Heinrich-Faust <juan.amaya.69@outlook.es>
2023-08-24 21:26:46 -0500

    home responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './core/layouts/header-components/header';
 import Footer from './core/layouts/footer-components/footer';
+import Cursos from './pages/Home/Home'
 
 function App() {
   return (
     <div>
       <Header />
-      {/* Content */}
-      <Footer /> 
+      <Cursos/>
+     
+      <Footer /> {}
     </div>
   );
 }
