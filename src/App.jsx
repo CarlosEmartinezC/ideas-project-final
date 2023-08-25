417d539adef878775664349b3453e048479bdb78
Heinrich-Faust <juan.amaya.69@outlook.es>
2023-08-24 21:04:12 -0500

    home

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './core/layouts/header-components/header';
 import Footer from './core/layouts/footer-components/footer';
-import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
-      {<Home/>}
+      {/* Content */}
       <Footer /> 
     </div>
   );
