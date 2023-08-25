b1a683b58e96f617afad87935a3baed161a1f613
Juanpak12 <juanpak12@gmail.com>
2023-08-24 21:26:24 -0500

    se finaliza css de header con bootstrap

--- src/core/layouts/header-components/LogoHeader.jsx
+++ src/core/layouts/header-components/LogoHeader.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from '../../../assets/img/header/LogoHorizontal.png';
+import './LogoHeader.scss';
+
 
 const LogoHeader = () => {
   return (
-    <div className="logoHeader">
+    <div className="logoHeader" id="LogoHeader" >
       <img src={logo} alt="Logo" />
     </div>
   );
