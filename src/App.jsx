9f716fc3418e2ec97a3a695af7d3688c8ad33458
neyryvargasd <neyry007@gmail.com>
2023-08-24 23:34:51 -0500

    solucionando conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Header from './core/layouts/header-components/header';
 import Footer from './core/layouts/footer-components/footer';
 import Cursos from './pages/Cursos/Cursos'
 import Slider from './pages/Cursos/components/slider';
+import Contact from './pages/Contact/Contact'
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
-      <Slider />
-      <Cursos/>
-     
+       <Home/>
       <Footer /> {}
     </div>
   );
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,80 +1,75 @@
+import './Contact.css';
+
 import React, { useRef, useState } from 'react';
-import './Contact.css'
-import { MdOutlineEmail } from 'react-icons/md';
+
+
 import emailjs from 'emailjs-com';
 
+
+
 const Contact = () => {
-    const [message, setMessage] = useState(false);
-    const formRef = useRef();
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setMessage(true);
-      emailjs
-        .sendForm(
-          'service_k2qawqh',
-          'template_c6rkpn6',
-          formRef.current,
-          'X7K7ebhIeOy3YwHki'
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-  
-      e.target.reset();
-    };
-    return (
+  const [message, setMessage] = useState(false);
+  const formRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+    emailjs
+      .sendForm(
+        'service_1jmfh8n',
+        'template_7oqg7vj',
+        formRef.current,
+        '3inYLJhelhH5DgS3k'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+      };
+      return (
         <div className='Contact'>
-        
-      <section id="contact">
-        <h5>Ponerse en contacto</h5>
-        <h5>
-       Recibo sus mensajes y responderé lo antes posible si se proporciona el correo electrónico válido 
-        </h5>
-        <h2>Contáctame</h2>
-        <div className="container contact__container">
-          <div className="contact__options">
-            <article className="contact__option">
-              <MdOutlineEmail className="contact__option-icon" />
-              <h4>email</h4>
-              <h5>tejer_ideas@gmail.com</h5>
-              <a href="mailto:mekomerikomg@gmail.com">Send a message</a>
-            </article>
+        <section id="contact">
+          <h5>Ponerse en contacto</h5><br/>
+          <h5>
+         Recibiremos sus mensajes y responderemoslo antes posible<br/>
+          si se proporciona el correo electrónico válido
+          </h5>
+          <h2>Contáctame</h2>
+          <div className="container contact__container">
+            
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Your Full Name"
+                name="user_name"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Your Email"
+                name="user_email"
+                required
+              />
+              <textarea
+                placeholder="Your message"
+                rows="7"
+                name="message"
+                required
+              ></textarea>
+              <button type="submit" className="btn btn">
+                Send Message
+              </button>
+              {message && <span>gracias, por enviarnos tu mensaje:)</span>}
+            </form>
           </div>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Your Full Name"
-              name="user_name"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Your Email"
-              name="user_email"
-              required
-            />
-            <textarea
-              placeholder="Your message"
-              rows="7"
-              name="message"
-              required
-            ></textarea>
-            <button type="submit" className="btn btn-primary">
-              Send Message
-            </button>
-            {message && <span>Thanks, I'll reply ASAP </span>}
-          </form>
+        </section>
         </div>
-      </section>
-     
-      </div>
-     
-    );
-  };
-  
-  export default Contact;
+      );
+    };
+    
+    export default Contact;
