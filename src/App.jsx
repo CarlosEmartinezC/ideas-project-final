f5619c5866d1528a08fa26d53568aa5266f103ea
Heinrich-Faust <juan.amaya.69@outlook.es>
2023-08-24 21:00:15 -0500

    hello my hommies

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './core/layouts/header-components/header';
 import Footer from './core/layouts/footer-components/footer';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
-      {/* Content */}
+      {<Home/>}
       <Footer /> 
     </div>
   );
