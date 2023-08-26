47d27d3ab2c31612147b4e7c24fde3060c3a3f4a
JSebastian08 <juansebastian0197@hotmail.com>
2023-08-25 22:24:00 -0500

    Arreglando problemas del slider y modificaciones al responsive de la pagina sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './core/layouts/header-components/header';
+import Carrusel from './pages/Cursos/components/slider';
 import Footer from './core/layouts/footer-components/footer';
 import Nosotros from './pages/Sobre-nosotros/nosotros';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Carrusel />
       <Nosotros/>
       <Footer /> {}
     </div>
