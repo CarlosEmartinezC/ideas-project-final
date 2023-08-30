efaf168dca5b4e753aee820df27dce9f822b91d4
Heinrich-Faust <juan.amaya.69@outlook.es>
2023-08-30 14:16:12 -0500

    pull

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,6 @@
 import './Contact.css';
 
 import React, { useRef, useState } from 'react';
-
 import Imagen from '../../recursos/contact1.png'
 import emailjs from 'emailjs-com';
 
@@ -35,14 +34,14 @@ const Contact = () => {
         <div className='Contact'>
          <img src={Imagen} alt='imagen1'></img>
         <section id="contact">
-          <h5>Ponerse en contacto</h5>
+        <h2>Contáctanos</h2>
           <h5>
           Estamos aquí para escucharte.<br/>
            Tu mensaje es importante para nosotros y haremos todo lo posible por responder tan pronto como sea posible.<br/>
            Por favor, asegúrate de proporcionar un correo electrónico válido para que podamos ponernos en contacto contigo.<br/>
             ¡Gracias por ayudarnos a mejorar!
           </h5>
-          <h2>Contáctanos</h2>
+        
           <div className="container contact__container">
             
             <form ref={formRef} onSubmit={handleSubmit}>
@@ -67,7 +66,13 @@ const Contact = () => {
               <button type="submit" className="btn btn">
                 Enviar mensaje
               </button>
-              {message && <span>gracias, por enviarnos tu mensaje</span>}
+              {message && <span>gracias, por enviarnos tu mensaje<br/>
+              Tu privacidad es esencial para nosotros.<br/>
+               En Fundacion tejer ideas, utilizamos tu correo electrónico solo<br/>
+                para los propósitos específicos que nos proporcionaste.<br/>
+                 No vendemos ni compartimos tu información.<br/>
+                 Siempre respetamos las regulaciones de privacidad y<br/>
+                  te mantendremos informado si usamos tus datos para nuevos fines.</span>}
             </form>
           </div>
         </section>
