de8b5b385b3377661f09ab95323070624402fdbc
Heinrich-Faust <juan.amaya.69@outlook.es>
2023-08-30 14:16:12 -0500

    pull

---
+++ public/Cursos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cursoss from "./components/cursoss";
+
+function Curso (){
+    return(
+        <>
+        <Cursoss/>
+
+        </>
+    )
+}
+
+export default Curso;
